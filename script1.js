03292d778c89af1839b8c05906da4517b053f3b0
VyshnaviIyengar <151837697+VyshnaviIyengar@users.noreply.github.com>
2024-05-03 19:13:36 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,19 @@
+﻿function downloadResume() {
+   
+    const resumeUrl = "resume.pdf";
+
+    
+    const link = document.createElement("a");
+    link.href = resumeUrl;
+    link.download = "Vyshnavi_Resume.pdf"; 
+    link.target = "_blank";
+
+
+    document.body.appendChild(link);
+    link.click();
+
+    
+    document.body.removeChild(link);
+}
+
+
